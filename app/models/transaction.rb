e9d300fc444f407e2560ea7ff3283947c055f5de
Tinus Wagner <tinus@3months.com>
2017-06-11 18:37:48 +1200

    refined payments

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -8,26 +8,28 @@ class Transaction < ApplicationRecord
     (merchant.fee * 100).to_i
   end
 
+  # Total amount converted to cents
   def total_amount
     (total * 100).to_i
   end
 
-  def charged_fee
-    total_amount * merchant.fee
-  end
-
-  def description(charge)
-    "#{user.email} just bought a #{item_bought.title} for $#{charge.to_s}, from #{merchant.title}"
-  end
-
+  # Credit converted to cents
   def credit_in_cents
     (user.credit * 100).to_i
   end
 
-  # Description inserted on charge that is covered by colo platform.
-  # takes one arg which should be the total amount so we can visualise what portion of charge is covered.
+  # Final fee in cents
+  def charged_fee(amount)
+    (amount * merchant.fee)
+  end
+
+  def description(amount)
+    "#{user.email} just bought a #{item_bought.title} for $#{amount/100}, from #{merchant.title} for transaction ID: #{id}"
+  end
+
+  # Description inserted on charge that is covered by platform.
   def credit_description(amount)
-    "Credit Transaction where platform credit covers #{total} of the original #{total} for transaction ID: #{id} -- #{user.email} just bought a #{item_bought.title} for $#{total.to_s}, from #{merchant.title}"
+    "Credited Transaction where platform credit covers $#{(amount/100)} of the original $#{total} for transaction ID: #{id} -- #{user.email} just bought a #{item_bought.title} for $#{total}, from #{merchant.title}"
   end
 
   def item_bought
@@ -35,51 +37,48 @@ class Transaction < ApplicationRecord
   end
 
   def charge_with_credit_check
+    # User has no credit so should be charged the full amount
+    make_charge(total_amount, user.stripe_customer_id, description(total_amount), charged_fee(total_amount), true) unless user.credit?
+
+    # Check if user has credit
     if user.credit?
+      # The user has enough credit to partially cover the total_amount
       if credit_in_cents - total_amount < 0
-        # The user has enough creidt to partially cover the fee
-        customer_charge_after_deduction = total_amount - credit_in_cents
-
-        # The portion of the charge the platform will cover
-        platform_charge = total_amount - customer_charge_after_deduction
-
+        customer_charge = total_amount - credit_in_cents
+        platform_charge = total_amount - customer_charge
         # We now have to make two seperate charges
-        # These charges are split between customer and platform
-        # We do not charge a fee on the platform charge!
-        make_charge(platform_charge, credit_description(platform_charge), ENV['MASTER_CREDIT_ACCOUNT'], 0, false)
-
-        # The remainder that the customer must pay
-        make_charge(customer_charge_after_deduction, user.stripe_customer_id, description(customer_charge_after_deduction), charged_fee, true)
-
-        # Update user remaining credit to 0
-        user.update_attribute(:credit, 0)
+        make_charge(platform_charge, ENV['MASTER_CREDIT_ACCOUNT'], credit_description(platform_charge), 0, false)
+        make_charge(customer_charge, user.stripe_customer_id, description(customer_charge), charged_fee(customer_charge), true)
+        update_user_credit(0)
       else
         # The user has enough credit to cover the entire charge
         credit_remaining_after_deduction = user.credit - total_amount
-
         platform_charge = total_amount
-        # Since the entire charge can be covered by credit
-        # We only create one charge
-        make_charge(platform_charge, ENV['MASTER_CREDIT_ACCOUNT'], credit_description(platform_charge),0 , false)
-
-        # Update users remaining credit
-        user.update_attribute(:credit, credit_remaining_after_deduction)
+        make_charge(platform_charge, ENV['MASTER_CREDIT_ACCOUNT'], credit_description(platform_charge), 0, false)
+        update_user_credit(credit_remaining_after_deduction)
       end
-    else
-      # User has no credit so should be charged the full amount
-      make_charge(total_amount, user.stripe_customer_id, description(total_amount), charged_fee, true)
     end
   end
 
-  def make_charge(charge, customer, description, fee, regular_charge)
+  # If User has more than $1 left in credit, we update remainder
+  def update_user_credit(amount)
+    if amount > 1
+      user.update_attribute(:credit, amount)
+    else
+      user.update_attribute(:credit, 0)
+    end
+  end
+
+  def make_charge(charge, origin, description, fee, customer_payment)
     charge = Stripe::Charge.create({
       # Total Amount user will be charged (in cents)
       amount: charge,
       # Currency of charge
       currency: 'USD',
-      # the applicant users Stripe Customer ID
+      # the origin of the charge
+      # e.g. customers Stripe Customer ID
       # expect format of "cus_0xxXxXXX0XXxX0"
-      customer: customer,
+      customer: origin,
       # Description of charge
       description: description,
       # Final Destination of charge (host standalone account)
@@ -92,15 +91,24 @@ class Transaction < ApplicationRecord
   # if the charge is successful, we'll receive a response in the charge object
   # We can then query that object via charge.paid
   # if true we can update our attribute
-  if charge.paid? && !regular_charge.blank?
-    after_charge_succeeded(charge)
+  if charge.paid?
+    after_charge_succeeded(charge, fee) if charge_was_made_by_user(charge)
+    after_credited_charge_succeeded(charge, fee) unless charge_was_made_by_user(charge)
   end
 
   rescue => e
     errors.add(:stripe_charge_error, "Could not create the charge. Info from Stripe: #{e.message}")
   end
 
-  def after_charge_succeeded(charge)
-    update_attributes(paid: true, stripe_charge: charge.id, fee_charged: charged_fee/100, total: charge.amount/100) if charge.paid?
+  def charge_was_made_by_user(charge)
+    return charge.customer == user.stripe_customer_id
+  end
+
+  def after_charge_succeeded(charge, fee)
+    update_attributes(paid: true, stripe_charge: charge.id, fee_charged: (fee.to_i/100), total: charge.amount/100)
+  end
+
+  def after_credited_charge_succeeded(charge, fee)
+    update_attributes(paid: true, stripe_charge: "credited purchase paid for by platform - #{charge.id}" , fee_charged: (fee.to_i/100), total: charge.amount/100)
   end
 end
